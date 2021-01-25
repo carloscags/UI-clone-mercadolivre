39069baae91878d360a0c1392a6188e5b9ffb222
Carlos <carlosaugustocf99@gmail.com>
2021-01-25 19:49:07 -0300

    Created product page and style page

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,77 @@
 import React from 'react';
-
-import { Container } from './styles';
+import tshirtImage from '../../assets/tshirt.png';
+import { Container, Row, Panel, Column, Gallery, Section, Description } from './styles';
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="#">Compartilhar</a>
+        <a href="#">Vender um igual</a>
+      </Row>
+
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img alt="T-shirt" src={tshirtImage} />
+          </Gallery>
+          <Info />
+        </Column>
+        <Column>
+          {/* <ProductAction />
+          <SellerInfo /> */}
+          
+          <WarrantySection />
+          <WarrantySection />
+          <WarrantySection /> 
+
+        </Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => {
+    return(
+      <Section>
+      <h4>Garantia</h4>
+
+      <div>
+        <span>
+          <p className="title">Compra Garantida com Lorem ipsum</p>
+          <p className="description">Receba o produto ou compra estornada</p>
+        </span>
+        <span>
+          <p className="title">Garantia do vendedor</p>
+          <p className="description">Sem garantia na compra</p>
+        </span>
+      </div>
+    </Section>
+    )
+    
+
+}
+
+const Info = () =>{
+    return(
+      <Description>
+        <h2>Descrição</h2>
+      
+        <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro.</p>
+        <br />
+        <br />
+        Itens inclusos : <br />
+        - 1x camiseta <br />
+        - 1x camiseta <br />
+        - 1x camiseta <br />
+        - 1x camiseta <br />
+        - 1x camiseta <br />
+        <br />
+    </Description>
+    )
+    
+
+}
 
 export default Product;
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
 
+
+export const Container = styled.div`
+    
 `;
+
+export const Row = styled.div``;
+
+export const Panel = styled.div``;
+
+export const Column = styled.div``;
+
+export const Gallery = styled.div``;
+
+export const Section = styled.div``;
+
+export const Description= styled.div``;
