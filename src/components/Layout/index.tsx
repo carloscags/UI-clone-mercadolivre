7ff6711b4b7b41c2baf4c1ada7e2642b1071f24b
Carlos <carlosaugustocf99@gmail.com>
2021-01-27 19:59:43 -0300

    ProductsAction styled finished

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Product from '../Product'
 import { Container, Wrapper } from './styles';
+import Header from '../Header';
+import Footer from '../Footer';
+
 
 const Layout: React.FC = () => {
   return (
     <Container>
-      {/* <Header /> */}
+      <Header />
       <Wrapper>
         <Product />
       </Wrapper>
-      {/* <Footer /> */}
+      <Footer />
     </Container>
   );
 };
